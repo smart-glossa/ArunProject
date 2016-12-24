eae794527ab4984dbccff7185a173312e90960ba
Product Reviewer <admin@smartglossa.com>
2016-12-24 16:25:37 +0530

    Changed Arun proejct as new maven project

---
+++ arun/src/main/java/com/smartglossa/arun/BillConstant.java
@@ -0,0 +1,8 @@
+package com.smartglossa.arun;
+
+public class BillConstant {
+
+	public static String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
+	public static String USERNAME = System.getProperty("mysql.username");
+	public static String PASSWORD = System.getProperty("mysql.password");
+}
